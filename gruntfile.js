f71ded6ae1ef868524d40395655bac9e6b885248
Angvish Shaw <angvishvish@gmail.com>
2015-03-04 17:40:09 +0530

    updating the index file

--- gruntfile.js
+++ gruntfile.js
@@ -8,22 +8,27 @@ module.exports = function(grunt) {
           'bower_components/html5-boilerplate/js/vendor/modernizr-2.6.2.min.js',
           'bower_components/angular/angular.js',
           'bower_components/angular-route/angular-route.js',
+          'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+          'bower_components/angular-resource/angular-resource.js',
+          'bower_components/angular-bootstrap/ui-bootstrap.js',
+          'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+          'assets/jquery.js',
+          'assets/highcharts.js',
+          'assets/highcharts-exporting.js',
           'app.js',
           'github/github.js',
           'github/github-user.js',
           'github/github-repo.js',
-          'github/github-repo-details.js'
+          'github/github-repo-details.js',
+          'repo-language.js'
         ],
         dest: 'app/<%= pkg.name %>.add.js'
       }
     },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
       dist: {
         files: {
-          '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          'github-viewer.min.js': ['<%= concat.dist.dest %>']
         }
       }
     },
